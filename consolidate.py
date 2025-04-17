d68086f8930a3c9d70c83e6b9dc28221af8c2e93
Andrewishere502 <andrewdberntson@gmail.com>
2025-04-16 23:17:31 -0500

    Add option to search for zipped origin_dirs if they initially can not be found. Also refactor a bit.

--- consolidate.py
+++ consolidate.py
@@ -3,13 +3,12 @@ one data folder with all images and a meta data CSV file tracking each
 image's origin folder (i.e. which "run" it came from, e.g. AutoCamRun3) and original file name within that run.
 '''
 
+import argparse
+from collections import OrderedDict
+import pathlib
+import zipfile
 
 import pandas as pd
-
-import argparse
-import pathlib
-from collections import OrderedDict
-
 
 def save_df_csv(df: pd.DataFrame, path: pathlib.Path) -> None:
     '''Save a given DataFrame object as a CSV file at the desired path.
@@ -30,6 +29,24 @@ def add_column(df: pd.DataFrame, new_column_name: str) -> pd.DataFrame:
     if new_column_name in df.columns:
         raise ValueError(f'Cannot add duplicate column {new_column_name} to dataframe')
     return pd.concat([df, pd.DataFrame(columns=[new_column_name])], axis=0)
+
+
+def try_unzip(origin_dir: pathlib.Path) -> bool:
+    '''Attempt to unzip origin_dir.zip. Return True if successful, False
+    otherwise.
+    '''
+    # Unzip origin_dir.zip if it exists
+    origin_dir_zip = origin_dir.with_suffix('.zip')
+    if origin_dir_zip.exists():
+        with zipfile.ZipFile(origin_dir_zip, 'r') as zip_file:
+            # Include all members except for that pesky __MACOSX
+            # directory
+            members = [member for member in zip_file.namelist()
+                       if member[:2] != '__']
+            zip_file.extractall(members=members)
+            return True
+    else:
+        return False
 
 
 INDEX_COL = 'ID'    # Unique identifier for each image
@@ -63,12 +80,18 @@ parser.add_argument('new_dir',
 
 # Add optional arguments
 parser.add_argument('-t', '--tank_id',
-                    default=False,  # If not specified, will be False
+                    default=False,  # If not specified will be False
                     help='Tank ID to use for all images'
                     )
 parser.add_argument('-f', '--filter',
-                    action='store_true',  # If not specified, will be False, otherwise true
+                    type=int,  # If not specified will be None
+                    choices=(0, 1),  # Must be either 0 or 1 if set, bool is NOT SUPPORTED
                     help='If the filter can be seen in any of the images being consolidated'
+                    )
+parser.add_argument('-z', '--check_zip',
+                    action='store_true',  # Default False, set to True if specified
+                    help='If a specified origin directory is not found,' \
+                         'check for a zip file with the same name'
                     )
 
 # Parse the command line arguments
@@ -85,10 +108,10 @@ meta_file = args.new_dir / 'metadata.csv'
 # Create the meta file with an appropriate header if it doesn't already
 # exist.
 if not meta_file.exists():
-    print(f'Creating metadata.csv file.')
     with open(meta_file, 'w') as file:  # Create metadata file
         header = INDEX_COL + ',' + ','.join(COLUMNS)
         file.write(header + '\n')  # Add in the header
+    print(f'Created metadata.csv file.')
 
 
 # Load the data, using the first column (column index 0) as the index
@@ -109,11 +132,22 @@ for column in COLUMNS:
 # Take each image from its directory, record its metadata in
 # metadata.csv, and move it to the data directory
 for origin_dir in args.origin_dirs:
+    # Ensure the origin_dir specified exists
     if not origin_dir.exists():
-        print(f'Skipping {origin_dir}: Unable to locate directory')
-        continue
+        # If check_zip option is True, look for a zip file of the same
+        # name (but with a .zip suffix).
+        if args.check_zip:
+            if try_unzip(origin_dir):
+                print(f'Unzipped {origin_dir}.zip to create {origin_dir}.')
+            else:
+                print(f'Skipping {origin_dir}: Unable to locate directory and no zip file was found')
+        else:
+            print(f'Skipping {origin_dir}: Unable to locate directory')
+            continue
 
+    # Ensure the origin_dir specified is a directory
     if not origin_dir.is_dir():
+        # Warn user if the path they specified was not a directory
         print(f'Skipping {origin_dir}: Not a directory')
         continue
 
@@ -124,14 +158,19 @@ for origin_dir in args.origin_dirs:
     # Set values that will be the same for all rows in this directory
     row['OriginDir'] = origin_dir
     row['NewDir'] = args.new_dir
-    if args.tank_id:  # Set tank ID, if specified
+    # Set tank ID, if specified
+    if args.tank_id:
         row['TankID'] = args.tank_id
+    # Set filter (visible or not), if specified
+    if args.filter != None:
+        row['Filter'] = args.filter
 
+    # Iterate over each image in this directory
     for img_orgin_name in origin_dir.iterdir():
         # Include only the file name, not its path
         row['OriginName'] = img_orgin_name.name
 
-        # Do not give images #0: VSCode won't open 00000000.JPG
+        # Do not give images #0: VSCode may get mad at 00000000.JPG
         im_num = len(meta_df) + 1
         img_new_name = pathlib.Path(f'{im_num:08d}' + img_orgin_name.suffix)
         row['NewName'] = img_new_name
@@ -147,3 +186,12 @@ for origin_dir in args.origin_dirs:
         # Yes this is inefficient, but it ensures every image has
         # its meta data saved in the event of an error.
         save_df_csv(meta_df, meta_file)
+
+    # Delete the directory we just moved all the images from, throws
+    # an error if the directory isn't empty for some reason.
+    try:
+        origin_dir.rmdir()
+        print(f'Removed {origin_dir}')
+    except OSError as e:
+        # Just warn the user and skip deletion
+        print(f'Unable to remove {origin_dir} after moving images: Directory not empty')
